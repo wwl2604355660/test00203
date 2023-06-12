115d73eafc458e6750a29d103b32f89b855efc34
WWL2604355660 <2604355660@qq.com>
2023-06-12 09:24:52 +0800

    222

--- src/main/java/com/springmvc/controller/LController.java
+++ src/main/java/com/springmvc/controller/LController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
-
+//计算两个复数,这个只是练习，不是真的题
 @Controller
 public class LController {
 
@@ -31,7 +31,7 @@ public class LController {
         int suby = intArray1[1] - intArray2[1];
         int mulx = intArray1[0] * intArray2[0] - intArray1[1] * intArray2[1];
         int muly = intArray1[0] * intArray2[1] + intArray2[0] * intArray1[1];
-        double denominator = (int) (Math.pow(intArray2[1], 2) + Math.pow(intArray2[1], 2));
+        double denominator = (Math.pow(intArray2[1], 2) + Math.pow(intArray2[1], 2));
         double divx = (intArray1[0] * intArray2[0] + intArray1[1] * intArray2[1]) / denominator;
         double divy = (intArray1[1] * intArray2[0] - intArray1[0] * intArray2[1]) / denominator;
 
